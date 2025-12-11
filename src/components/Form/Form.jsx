d39e1d8a859772490c321b2d316b2253f1f9fafc
dvankevich <dvankevich@gmail.com>
2025-12-11 20:23:22 +0200

    Form component

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,7 +1,24 @@
-// import { FiSearch } from 'react-icons/fi';
+import { FiSearch } from 'react-icons/fi';
+import style from './Form.module.css';
 
-const Form = () => {
-  return <h2>Form</h2>;
+const Form = ({ onSubmit }) => {
+  return (
+    <>
+      <form className={style.form} onSubmit={onSubmit}>
+        <button className={style.button} type="submit">
+          <FiSearch size="16px" />
+        </button>
+
+        <input
+          className={style.input}
+          placeholder="What do you want to write?"
+          name="search"
+          required
+          autoFocus
+        />
+      </form>
+    </>
+  );
 };
 
 export default Form;
--- src/tabs/Todos.jsx
+++ src/tabs/Todos.jsx
@@ -1,9 +1,16 @@
-import Text from '../components/Text/Text';
+//import Text from '../components/Text/Text';
+import Form from '../components/Form/Form';
 
 const Todos = () => {
+  const addNewTodo = evt => {
+    evt.preventDefault();
+    console.log(evt.target.search.value); // має вивести значення інпуту під час сабміту форми
+    evt.target.reset();
+  };
   return (
     <>
-      <Text textAlign="center">There are no any todos ...</Text>
+      {/* <Text textAlign="center">There are no any todos ...</Text> */}
+      <Form onSubmit={addNewTodo} />
     </>
   );
 };
