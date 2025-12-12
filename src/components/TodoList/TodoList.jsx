3ddb49fba823062656942bdb1e0a1eb47258ee3b
dvankevich <dvankevich@gmail.com>
2025-12-12 18:09:47 +0200

    TodoList TodoListItem

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,5 +1,17 @@
-const TodoList = () => {
-  return <h3>TodoList</h3>;
+import Grid from '../Grid/Grid';
+import GridItem from '../GridItem/GridItem';
+import TodoListItem from '../TodoListItem/TodoListItem';
+
+const TodoList = ({ todoList }) => {
+  return (
+    <Grid>
+      {todoList.map(todoItem => (
+        <GridItem key={todoItem.id}>
+          <TodoListItem todoItem={todoItem} />
+        </GridItem>
+      ))}
+    </Grid>
+  );
 };
 
 export default TodoList;
--- src/tabs/Todos.jsx
+++ src/tabs/Todos.jsx
@@ -1,13 +1,22 @@
 import Text from '../components/Text/Text';
 import Form from '../components/Form/Form';
+import { useState } from 'react';
+import TodoList from '../components/TodoList/TodoList';
 
 const Todos = () => {
+  const [todos, setTodos] = useState([
+    { id: '1', text: 'Practice more' },
+    { id: '2', text: 'Get all tasks done on time' },
+    { id: '3', text: 'Work, Learn again and again' },
+  ]);
+
   const addNewTodo = inputValue => {
     console.log(inputValue); // має вивести значення інпуту під час сабміту форми
   };
   return (
     <>
       <Form onSubmit={addNewTodo} />
+      <TodoList todoList={todos} />
       <Text textAlign="center">There are no any todos ...</Text>
     </>
   );
