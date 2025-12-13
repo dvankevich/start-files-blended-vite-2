220fd4053f89cfc026700b7cd3877dee0e06aa99
dvankevich <dvankevich@gmail.com>
2025-12-13 19:46:12 +0200

    save todos to local storage

--- src/tabs/Todos.jsx
+++ src/tabs/Todos.jsx
@@ -1,18 +1,24 @@
 import Text from '../components/Text/Text';
 import Form from '../components/Form/Form';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import TodoList from '../components/TodoList/TodoList';
+import todosInit from '../todosInit.json';
+import { nanoid } from 'nanoid';
 
 const Todos = () => {
-  const [todos, setTodos] = useState([
-    { id: '1', text: 'Practice more' },
-    { id: '2', text: 'Get all tasks done on time' },
-    { id: '3', text: 'Work, Learn again and again' },
-  ]);
+  const [todos, setTodos] = useState(() => {
+    return JSON.parse(window.localStorage.getItem('todos')) ?? todosInit;
+  });
+
+  useEffect(() => {
+    window.localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
 
   const addNewTodo = inputValue => {
     console.log(inputValue); // має вивести значення інпуту під час сабміту форми
+    setTodos(prevTodos => [...prevTodos, { id: nanoid(), text: inputValue }]);
   };
+
   return (
     <>
       <Form onSubmit={addNewTodo} />
