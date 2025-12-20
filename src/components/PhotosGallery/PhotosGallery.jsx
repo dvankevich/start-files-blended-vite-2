b7917443fb7493076dcf7eb492ab33ba90478aa2
dvankevich <dvankevich@gmail.com>
2025-12-20 21:27:15 +0200

    Photos PhotosGallery PhotosGalleryItem

--- src/components/PhotosGallery/PhotosGallery.jsx
+++ src/components/PhotosGallery/PhotosGallery.jsx
@@ -1,4 +1,20 @@
-const PhotosGallery = () => {
-  return <h3>PhotosGallery</h3>;
+import Grid from '../Grid/Grid';
+import GridItem from '../GridItem/GridItem';
+import PhotosGalleryItem from '../PhotosGalleryItem/PhotosGalleryItem';
+
+const PhotosGallery = ({ images }) => {
+  console.log(images);
+
+  return (
+    <>
+      <Grid>
+        {images.map(image => (
+          <GridItem key={image.id}>
+            <PhotosGalleryItem image={image} />
+          </GridItem>
+        ))}
+      </Grid>
+    </>
+  );
 };
 export default PhotosGallery;
--- src/tabs/Photos.jsx
+++ src/tabs/Photos.jsx
@@ -1,9 +1,22 @@
 import Text from '../components/Text/Text';
+import Form from '../components/Form/Form';
+import { useEffect, useState } from 'react';
+import PhotosGallery from '../components/PhotosGallery/PhotosGallery';
+
+import images from '../apiService/photos.json';
 
 const Photos = () => {
+  const [query, setQuery] = useState('');
+
+  const getQuery = inputValue => {
+    setQuery(inputValue);
+  };
+
   return (
     <>
       <Text textAlign="center">Let`s begin search 🔎</Text>
+      <Form onSubmit={getQuery} />
+      {images.length > 0 && <PhotosGallery images={images} />}
     </>
   );
 };
